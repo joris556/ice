9095493bbf5e7d6c53f7687251ba9d0869078f51
Joris Lelieveld <j.r.j.lelieveld@students.uu.nl>
2020-05-26 10:34:54 +0100

    biesie verder

--- IceStatistieken/Form1.Designer.cs
+++ IceStatistieken/Form1.Designer.cs
@@ -33,6 +33,16 @@
             this.numTijd = new System.Windows.Forms.NumericUpDown();
             this.dropDag = new System.Windows.Forms.ComboBox();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dropCalBet = new System.Windows.Forms.ComboBox();
+            this.dropCallert = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.butAdd = new System.Windows.Forms.Button();
+            this.finalText = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.numTijd)).BeginInit();
             this.SuspendLayout();
             // 
@@ -40,7 +50,7 @@
             // 
             this.dropWie.Font = new System.Drawing.Font("Cambria", 20.25F);
             this.dropWie.FormattingEnabled = true;
-            this.dropWie.Location = new System.Drawing.Point(11, 33);
+            this.dropWie.Location = new System.Drawing.Point(12, 50);
             this.dropWie.Name = "dropWie";
             this.dropWie.Size = new System.Drawing.Size(321, 40);
             this.dropWie.TabIndex = 0;
@@ -51,7 +61,7 @@
             // 
             this.dropDoorWie.Font = new System.Drawing.Font("Cambria", 20.25F);
             this.dropDoorWie.FormattingEnabled = true;
-            this.dropDoorWie.Location = new System.Drawing.Point(12, 79);
+            this.dropDoorWie.Location = new System.Drawing.Point(12, 96);
             this.dropDoorWie.Name = "dropDoorWie";
             this.dropDoorWie.Size = new System.Drawing.Size(320, 40);
             this.dropDoorWie.TabIndex = 1;
@@ -66,7 +76,7 @@
             0,
             0,
             0});
-            this.numTijd.Location = new System.Drawing.Point(13, 125);
+            this.numTijd.Location = new System.Drawing.Point(12, 142);
             this.numTijd.Maximum = new decimal(new int[] {
             2400,
             0,
@@ -81,24 +91,132 @@
             0,
             0,
             0});
+            this.numTijd.ValueChanged += new System.EventHandler(this.numTijd_ValueChanged);
             // 
             // dropDag
             // 
             this.dropDag.Font = new System.Drawing.Font("Cambria", 20.25F);
             this.dropDag.FormattingEnabled = true;
-            this.dropDag.Location = new System.Drawing.Point(13, 170);
+            this.dropDag.Location = new System.Drawing.Point(12, 187);
             this.dropDag.Name = "dropDag";
             this.dropDag.Size = new System.Drawing.Size(319, 40);
             this.dropDag.TabIndex = 3;
             this.dropDag.Text = "Welke dag is het?";
+            this.dropDag.SelectedIndexChanged += new System.EventHandler(this.dropDag_SelectedIndexChanged);
             // 
             // dateTimePicker1
             // 
             this.dateTimePicker1.Font = new System.Drawing.Font("Cambria", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dateTimePicker1.Location = new System.Drawing.Point(13, 216);
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 233);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(319, 39);
             this.dateTimePicker1.TabIndex = 4;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cambria", 20.25F);
+            this.label1.Location = new System.Drawing.Point(339, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 32);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Vindert";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Cambria", 20.25F);
+            this.label2.Location = new System.Drawing.Point(339, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(151, 32);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Verstoppert";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Cambria", 20.25F);
+            this.label3.Location = new System.Drawing.Point(339, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 32);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Tijd";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Cambria", 20.25F);
+            this.label4.Location = new System.Drawing.Point(339, 190);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 32);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Dag";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Cambria", 20.25F);
+            this.label5.Location = new System.Drawing.Point(339, 238);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 32);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Datum";
+            // 
+            // dropCalBet
+            // 
+            this.dropCalBet.Font = new System.Drawing.Font("Cambria", 20.25F);
+            this.dropCalBet.FormattingEnabled = true;
+            this.dropCalBet.Location = new System.Drawing.Point(12, 278);
+            this.dropCalBet.Name = "dropCalBet";
+            this.dropCalBet.Size = new System.Drawing.Size(321, 40);
+            this.dropCalBet.TabIndex = 10;
+            this.dropCalBet.Text = "Gecalled of betrapt?";
+            this.dropCalBet.Visible = false;
+            this.dropCalBet.SelectedIndexChanged += new System.EventHandler(this.dropCalBet_SelectedIndexChanged);
+            // 
+            // dropCallert
+            // 
+            this.dropCallert.Font = new System.Drawing.Font("Cambria", 20.25F);
+            this.dropCallert.FormattingEnabled = true;
+            this.dropCallert.Location = new System.Drawing.Point(12, 324);
+            this.dropCallert.Name = "dropCallert";
+            this.dropCallert.Size = new System.Drawing.Size(321, 40);
+            this.dropCallert.TabIndex = 11;
+            this.dropCallert.Text = "Door wie?";
+            this.dropCallert.Visible = false;
+            this.dropCallert.SelectedIndexChanged += new System.EventHandler(this.dropCallert_SelectedIndexChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Cambria", 24.25F);
+            this.label6.Location = new System.Drawing.Point(12, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(177, 38);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Toevoegen:";
+            // 
+            // butAdd
+            // 
+            this.butAdd.Font = new System.Drawing.Font("Cambria", 20.25F);
+            this.butAdd.Location = new System.Drawing.Point(10, 392);
+            this.butAdd.Name = "butAdd";
+            this.butAdd.Size = new System.Drawing.Size(321, 40);
+            this.butAdd.TabIndex = 13;
+            this.butAdd.Text = "Toevoegen";
+            this.butAdd.UseVisualStyleBackColor = true;
+            this.butAdd.Click += new System.EventHandler(this.butAdd_Click);
+            // 
+            // finalText
+            // 
+            this.finalText.AutoSize = true;
+            this.finalText.Font = new System.Drawing.Font("Cambria", 14.25F);
+            this.finalText.Location = new System.Drawing.Point(12, 367);
+            this.finalText.Name = "finalText";
+            this.finalText.Size = new System.Drawing.Size(199, 22);
+            this.finalText.TabIndex = 14;
+            this.finalText.Text = "WieWatWaarWanneer?";
             // 
             // Form1
             // 
@@ -106,6 +224,16 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLight;
             this.ClientSize = new System.Drawing.Size(1305, 682);
+            this.Controls.Add(this.finalText);
+            this.Controls.Add(this.butAdd);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dropCallert);
+            this.Controls.Add(this.dropCalBet);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.dropDag);
             this.Controls.Add(this.numTijd);
@@ -113,8 +241,10 @@
             this.Controls.Add(this.dropWie);
             this.Name = "Form1";
             this.Text = "Ice statistieken";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.numTijd)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -125,6 +255,16 @@
         private System.Windows.Forms.NumericUpDown numTijd;
         private System.Windows.Forms.ComboBox dropDag;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox dropCalBet;
+        private System.Windows.Forms.ComboBox dropCallert;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button butAdd;
+        private System.Windows.Forms.Label finalText;
     }
 }
 
--- IceStatistieken/Form1.cs
+++ IceStatistieken/Form1.cs
@@ -12,19 +12,135 @@ namespace IceStatistieken
 {
     public partial class Form1 : Form
     {
+        //Bart, Eduardo, Mantas, Joris, Gini, Thijs, Martijn, Len, outsiders
+        private string[] names = { "Bart", "Eduardo", "Gini", "Joris", "Len", "Mantas", "Martijn", "Thijs", "Een outsider" };
+        private string[] dagen = { "Maandag", "Dinsdag", "Woensdag", "Donderdag", "Vrijdag", "Zaterdag", "Zondag" };
+        private string[] exOpties = { "Gecalled", "Betrapt" };
+        private string vindert = "", verstoppert = "", dag = "", datum = "", tijd = "", calBetrapper = "";
+        private bool called = false, betrapt = false;
+
         public Form1()
         {
             InitializeComponent();
+            dropWie.Items.AddRange(names);
+            dropDoorWie.Items.AddRange(names);
+            dropDag.Items.AddRange(dagen);
+            dropCalBet.Items.AddRange(exOpties);
+            dropCalBet.Visible = false;
+            dropCallert.Items.AddRange(names);
+        }
+
+        private void butAdd_Click(object sender, EventArgs e)
+        {
+            //launch query
+            reset();
+        }
+
+        private void UpdateFinalText()
+        {
+            if (called)
+                finalText.Text = "De Ice van " + vindert + " werd gecalled door " + calBetrapper + " om " + tijd + " op " + dag + " " + datum;
+            else if (betrapt)
+                finalText.Text = vindert + " werd betrapt tijdens het verstoppen door " + calBetrapper + " om " + tijd + " op " + dag + " " + datum;
+            else
+                finalText.Text = vindert + " vond de Ice van " + verstoppert + " om " + tijd + " op " + dag + " " + datum;
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            datum = dateTimePicker1.Value.ToString().Split()[0];
+            UpdateFinalText();
+        }
+
+        private void dropDag_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dag = dropDag.SelectedItem.ToString();
+            UpdateFinalText();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void reset()
+        {
+            vindert = "";
+            verstoppert = "";
+            dag = "";
+            datum = "";
+            tijd = "";
+            calBetrapper = "";
+            called = false;
+            betrapt = false;
+            dropDoorWie.Text = "Wie verstopte de Ice?";
+            dropWie.Text = "Wie vond de Ice?";
+            dropDag.Text = "Welke dag is het?";
+            dropCalBet.Text = "Gecalled of betrapt?";
+            dropCallert.Text = "Door wie?";
+            dropCallert.Visible = false;
+            dropCalBet.Visible = false;
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
+            vindert = dropWie.SelectedItem.ToString();
+            if (dropWie.SelectedItem == dropDoorWie.SelectedItem)
+            {
+                dropCalBet.Visible = true;
+                dropCallert.Visible = true;
+            }
+            else
+            {
+                dropCallert.Visible = false;
+                dropCalBet.Visible = false;
+            }
 
+            UpdateFinalText();
         }
 
         private void dropDoorWie_SelectedIndexChanged(object sender, EventArgs e)
         {
+            verstoppert = dropDoorWie.SelectedItem.ToString();
+            if (dropWie.SelectedItem == dropDoorWie.SelectedItem)
+            {
+                dropCalBet.Visible = true;
+                dropCallert.Visible = true;
+            }
+            else
+            {
+                dropCallert.Visible = false;
+                dropCalBet.Visible = false;
+            }
 
+            UpdateFinalText();
+        }
+
+        private void numTijd_ValueChanged(object sender, EventArgs e)
+        {
+            tijd = numTijd.Value.ToString();
+            UpdateFinalText();
+        }
+
+        private void dropCalBet_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (dropCalBet.SelectedItem.ToString() == "Gecalled")
+            {
+                betrapt = false;
+                called = true;
+            }
+            else if (dropCalBet.SelectedItem.ToString() == "Betrapt")
+            {
+                called = false;
+                betrapt = true;
+            }
+            UpdateFinalText();
+        }
+
+        private void dropCallert_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            calBetrapper = dropCallert.SelectedItem.ToString();
+            UpdateFinalText();
         }
     }
 }
